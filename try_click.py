6448af618c0e7379af44e78c8bfc96a44ee637cf
Mikko Pylkkänen <pylkkanen@gmail.com>
2022-02-09 14:33:03 +0200

    Rename files

---
+++ try_click.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Command line args with Click.
+
+See:
+- https://click.palletsprojects.com/
+
+Usage:
+    python3 try-cli.py --help
+    python3 try-cli.py --count 3 Nimi
+"""
+
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+if __name__ == '__main__':
+    hello()
