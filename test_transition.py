ff91fb3b93f8c0ebf27a3059a1e1ae61ff06a139
Mikko Pylkkänen <pylkkanen@gmail.com>
2022-02-19 17:31:24 +0200

    More tests

--- test_transition.py
+++ test_transition.py
@@ -3,3 +3,14 @@ import try_transition
 def test_transition_create():
     batman = try_transition.NarcolepticSuperhero("Batman")
     assert batman.state == 'asleep'
+
+def test_transition_wake_up():
+    batman = try_transition.NarcolepticSuperhero("Batman")
+    batman.wake_up()
+    assert batman.state == 'hanging out'
+
+def test_transition_distress_call():
+    batman = try_transition.NarcolepticSuperhero("Batman")
+    batman.distress_call()
+    batman.complete_mission()
+    assert batman.kittens_rescued == 1
